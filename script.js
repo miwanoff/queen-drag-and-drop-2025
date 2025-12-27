93347fcca0a06c1955d83aa3bafa970f33f500f5
miwanoff <marina.iva.noffa@gmail.com>
2025-12-27 12:21:17 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict";
+const cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+function shuffle(arr) {
+  let rand, temp;
+  for (let i = 0; i < arr.length; i++) {
+    rand = Math.floor(Math.random() * (i + 1));
+    temp = arr[rand];
+    arr[rand] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
+}
+
+function play(cards) {
+  let card = "";
+  console.log(shuffle(cards));
+
+  while (cards.length > 0) {
+    alert("Your turn!");
+    let n = prompt("Choose a card")
+    if (isNaN(n)){
+        return;
+    }
+    alert((cards[n]));
+    if (cards[n] === "Q") {
+      alert("Your win!");
+      return;
+    }
+    alert("My turn!");
+    let rand = Math.floor(Math.random() * cards.length);
+    alert((cards[rand]))
+    if (cards[rand] === "Q") {
+      alert("I win!");
+      return;
+    }
+  }
+}
+
+play(cards);
